8c5b338db7852eee6c6ea909ff57a9b4791b455f
jesse <jessejamesrafferty@gmail.com>
2023-07-09 23:39:33 +1200

    Moved PeripheralHook and Synchronization into the threading package

--- JavaMacro/src/macro/Main.java
+++ JavaMacro/src/macro/Main.java
@@ -4,7 +4,6 @@ import com.github.kwhat.jnativehook.NativeHookException;
 import macro.io.MacroFileReader;
 import macro.threading.PeripheralHook;
 import macro.threading.ScriptDispatcher;
-import macro.threading.ScriptExecutor;
 import macro.threading.Synchronization;
 
 import java.io.IOException;
@@ -25,7 +24,6 @@ public class Main {
         ExecutorService executor = Executors.newCachedThreadPool();
         Synchronization synchronization = new Synchronization();
         PeripheralHook peripheral = new PeripheralHook(synchronization, executor);
-        ScriptExecutor scriptExecutor = new ScriptExecutor(synchronization, executor);
-        ScriptDispatcher scriptDispatcher = new ScriptDispatcher(synchronization, executor, scriptExecutor);
+        ScriptDispatcher scriptDispatcher = new ScriptDispatcher(synchronization, executor);
     }
 }
