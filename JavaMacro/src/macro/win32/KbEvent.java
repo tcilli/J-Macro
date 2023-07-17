2350fd976563a5d7655fc96e5376ad11bceb2e15
jesse <jessejamesrafferty@gmail.com>
2023-07-17 23:50:11 +1200

    added some java doc

--- JavaMacro/src/macro/win32/KbEvent.java
+++ JavaMacro/src/macro/win32/KbEvent.java
@@ -30,6 +30,29 @@ package macro.win32;
  * Scan codes are specific to the hardware and may differ across different keyboard models,
  * while virtual keys provide a consistent and platform-independent way of referring to keys.
  * </p>
+ * ///////////////////////////////////////////////////////////////////////////<p></p>
+ * dwFlags Bits Description 0 - 7 <p></p>
+ *
+ * 0 Specifies whether the key is an extended key,
+ * such as a function key or a key on the numeric keypad.
+ * The value is 1 if the key is an extended key; otherwise, it is 0.<p></p>
+ *
+ * 1 Specifies whether the event was injected from a process running at lower integrity level.
+ * The value is 1 if that is the case; otherwise, it is 0. Note that bit 4 is also set whenever bit 1 is set.<p></p>
+ *
+ * 2-3 Reserved.<p></p>
+ *
+ * 4 Specifies whether the event was injected.
+ * The value is 1 if that is the case; otherwise, it is 0.
+ * Note that bit 1 is not necessarily set when bit 4 is set.<p></p>
+ *
+ * 5 The context code. The value is 1 if the ALT key is pressed; otherwise, it is 0.<p></p>
+ *
+ * 6 Reserved<p></p>
+ *
+ * 7 The transition state. The value is 0 if the key is pressed and 1 if it is being released.<p></p>
+ *  ///////////////////////////////////////////////////////////////////////////
+ *
  */
 public class KbEvent {
 
@@ -43,42 +66,29 @@ public class KbEvent {
     public static final int KEYUP = 0x0002;
 
     //The virtual code of the shift key (Windows mapping).
-    public static final int VK_SHIFT = 0x10;
+    public static final byte VK_SHIFT = 0x10;
 
     //The shift key flag.
-    public static final int SHIFT_KEY = 0x01;
+    public static final byte SHIFT_KEY = 0x01;
 
     /**
      * Simulates keystrokes by sending virtual keycode to the active window.
-     * characters that require the shift key to be pressed are sent with the shift key pressed.
-     * <p><b>dwFlags Bits Description</b></p>
-     * <p><b>0)</b>	Specifies whether the key is an extended key,
-     * such as a function key or a key on the numeric keypad.
-     * The value is 1 if the key is an extended key; otherwise, it is 0.</p>
-     * <p><b>1)</b>	Specifies whether the event was injected from a process running at lower integrity level.
-     * The value is 1 if that is the case; otherwise, it is 0. Note that bit 4 is also set whenever bit 1 is set.</p>
-     * <p><b>2-3)</b>	Reserved.
-     * <p><b>4)</b>	Specifies whether the event was injected.
-     * The value is 1 if that is the case; otherwise, it is 0.
-     * Note that bit 1 is not necessarily set when bit 4 is set.</p>
-     * <p><b>5)</b>	The context code. The value is 1 if the ALT key is pressed; otherwise, it is 0.</p>
-     * <p><b>6)</b>	Reserved.</p>
-     * <p><b>7)</b>	The transition state. The value is 0 if the key is pressed and 1 if it is being released.</p>
-     *
+     * <p>Refer to <a href="https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-kbdllhookstruct">
+     *     winuser-kbdllhookstruct </a> for more information
+     * </p>
      * @param charArray The array of characters to send.
      */
     public static void send_characters(char[] charArray) {
 
         for (char c : charArray) {
 
-            int result = KbInterface.winUser32.VkKeyScan(c);
+            int scan = KbInterface.winUser32.VkKeyScan(c);
 
-            //Test the extended-key flag
-            int virtualKeyCode = result & 0xFF;
+            byte virtualKeyCode = (byte) (scan & 0xFF);
 
-            int shiftState = result >> 8;
+            int extended = scan >> 8;
 
-            boolean shiftRequired = (shiftState & SHIFT_KEY) != 0;
+            boolean shiftRequired = (extended & SHIFT_KEY) != 0;
 
             int dwFlags = 0;
 
@@ -87,24 +97,24 @@ public class KbEvent {
              */
             if (shiftRequired) {
                 dwFlags |= SHIFT_DOWN;
-                KbInterface.winUser32.keybd_event((byte) VK_SHIFT, (byte) 0, KEYDOWN, 0);
+                KbInterface.winUser32.keybd_event(VK_SHIFT, (byte) 0, KEYDOWN, 0);
             }
 
             /*
              * Pressing the key down
              */
-            KbInterface.winUser32.keybd_event((byte) virtualKeyCode, (byte) 0, dwFlags, 0);
+            KbInterface.winUser32.keybd_event(virtualKeyCode, (byte) 0, dwFlags, 0);
 
             /*
              * Releasing the key up
              */
-            KbInterface.winUser32.keybd_event((byte) virtualKeyCode, (byte) 0, dwFlags | KEYUP, 0);
+            KbInterface.winUser32.keybd_event(virtualKeyCode, (byte) 0, dwFlags | KEYUP, 0);
 
             /*
              * Releasing the modifier keys.
              */
             if (shiftRequired) {
-                KbInterface.winUser32.keybd_event((byte) VK_SHIFT, (byte) 0, KEYUP, 0);
+                KbInterface.winUser32.keybd_event(VK_SHIFT, (byte) 0, KEYUP, 0);
             }
         }
     }
